06403bd6c9b0b3d5861f280b066019d029aec186
azaslonov <zaslonov.alexander@gmail.com>
2019-02-13 22:20:51 -0800

    minor fix for border with no width

--- src/plugins/borderStylePlugin.ts
+++ src/plugins/borderStylePlugin.ts
@@ -6,10 +6,10 @@ export class BorderStylePlugin extends StylePlugin {
 
     public async contractToJss(contract: BorderContract): Promise<Object> {
         const result = {
-            borderTop: contract.top ? [contract.top.width, contract.top.style, contract.top.color] : "none",
-            borderLeft: contract.left ? [contract.left.width, contract.left.style, contract.left.color] : "none",
-            borderRight: contract.right ? [contract.right.width, contract.right.style, contract.right.color] : "none",
-            borderBottom: contract.bottom ? [contract.bottom.width, contract.bottom.style, contract.bottom.color] : "none",
+            borderTop: contract.top && contract.top.width ? [contract.top.width, contract.top.style, contract.top.color] : "none",
+            borderLeft: contract.left && contract.left.width ? [contract.left.width, contract.left.style, contract.left.color] : "none",
+            borderRight: contract.right && contract.right.width ? [contract.right.width, contract.right.style, contract.right.color] : "none",
+            borderBottom: contract.bottom && contract.bottom.width ? [contract.bottom.width, contract.bottom.style, contract.bottom.color] : "none",
         };
 
         return result;
