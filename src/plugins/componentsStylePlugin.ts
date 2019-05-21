324dde4ed65e5ba2a147a3b0a445f30e691b300c
azaslonov <zaslonov.alexander@gmail.com>
2019-05-20 23:17:20 -0700

    fixes for nested components in style guide

--- src/plugins/componentsStylePlugin.ts
+++ src/plugins/componentsStylePlugin.ts
@@ -1,20 +1,32 @@
+import * as Utils from "@paperbits/common/utils";
 import { StylePlugin } from "./stylePlugin";
 import { StyleCompiler } from "..";
 
+
+
 export class ComponentsStylePlugin extends StylePlugin {
-    public readonly name = "components";
+    public readonly name: string = "components";
 
     constructor(private readonly styleCompiler: StyleCompiler) {
         super();
     }
 
-    public async contractToJss(componentsConfig): Promise<Object> {
+    public async contractToJss(componentsConfig: any): Promise<Object> {
         const result = {};
 
         for (const componentName of Object.keys(componentsConfig)) {
-            const componentVariationConfig = componentsConfig[componentName];
-            const pluginRules = await this.styleCompiler.getVariationClasses(componentVariationConfig, componentName, null, true);
-            Object.assign(result, pluginRules);
+            const componentConfig = componentsConfig[componentName];
+
+            const defaultComponentStyles = await this.styleCompiler.getVariationClasses(componentConfig["default"], componentName, "default", true);
+
+            for (const variationName of Object.keys(componentConfig)) {
+                if (variationName === "default") continue;
+                const variationStyles = await this.styleCompiler.getVariationClasses(componentConfig[variationName], componentName, variationName, true);
+                const key = `& .${componentName}-${variationName}`;
+                defaultComponentStyles[componentName] = { ...defaultComponentStyles[componentName], [`&.${componentName}-${variationName}`]: variationStyles[key] };
+            }
+
+            Utils.assign(result, defaultComponentStyles);
         }
 
         return result;
