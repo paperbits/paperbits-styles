597814c0d1080303da46e06f15dd44e264cbdffb
azaslonov <zaslonov.alexander@gmail.com>
2019-07-27 10:03:39 -0700

    Refactoring around type usage. Cleaning up deprecated artifacts.

--- src/ko/bindingHandlers/bindingHandlers.styled.ts
+++ src/ko/bindingHandlers/bindingHandlers.styled.ts
@@ -1,36 +1,34 @@
 import * as ko from "knockout";
+import { StyleModel } from "@paperbits/common/styles";
 
 
-export class StyledBindingHandler {
-    constructor() {
-        ko.bindingHandlers["styled"] = {
-            update: async (element: HTMLElement, valueAccessor) => {
-                const styleConfig = ko.unwrap(valueAccessor());
+ko.bindingHandlers["styled"] = {
+    update: async (element: HTMLElement, valueAccessor) => {
+        const styleModel: StyleModel = ko.unwrap(valueAccessor());
 
-                if (!styleConfig) {
-                    return;
-                }
-                const cssObservable = ko.observable();
+        if (!styleModel) {
+            return;
+        }
+        
+        const cssObservable = ko.observable();
 
-                let styleElement = element.ownerDocument.getElementById(styleConfig.key);
+        let styleElement = element.ownerDocument.getElementById(styleModel.key);
 
-                if (styleConfig.css) {
-                    if (!styleElement) {
-                        styleElement = element.ownerDocument.createElement("style");
-                        styleElement.id = styleConfig.key;
-                        element.parentElement.insertBefore(styleElement, element);
-                    }
-                    
-                    styleElement.innerHTML = await styleConfig.css;
-                }
-                else if (styleElement) {
-                    styleElement.remove();
-                }
-
-                cssObservable(styleConfig.classNames);
-
-                ko.applyBindingsToNode(element, { css: cssObservable }, null);
+        if (styleModel.css) {
+            if (!styleElement) {
+                styleElement = element.ownerDocument.createElement("style");
+                styleElement.id = styleModel.key;
+                element.parentElement.insertBefore(styleElement, element);
             }
-        };
+
+            styleElement.innerHTML = await styleModel.css;
+        }
+        else if (styleElement) {
+            styleElement.remove();
+        }
+
+        cssObservable(styleModel.classNames);
+
+        ko.applyBindingsToNode(element, { css: cssObservable }, null);
     }
-}
+};
--- src/styles.module.ts
+++ src/styles.module.ts
@@ -6,16 +6,16 @@
  * Use of this source code is governed by a Commercial license that can be found in the LICENSE file and at https://paperbits.io/license/mit.
  */
 
+import "./ko/bindingHandlers/bindingHandlers.styled";
 import { IInjectorModule, IInjector } from "@paperbits/common/injection";
 import { StyleService, StyleCompiler } from "./";
 import { StylePublisher } from "./publishing/stylePublisher";
-import { StyledBindingHandler } from "./ko/bindingHandlers/bindingHandlers.styled";
+
 
 export class StyleModule implements IInjectorModule {
     public register(injector: IInjector): void {
         injector.bindSingleton("styleService", StyleService);
         injector.bindToCollection("publishers", StylePublisher);
         injector.bindSingleton("styleCompiler", StyleCompiler);
-        injector.bindToCollection("autostart", StyledBindingHandler);
     }
 }
