c474b7aef0c049245c2f9b33db4aa0732f826e5a
Alexander Zaslonov <zaslonov.alexander@gmail.com>
2019-09-04 15:09:32 -0700

    Switched to new npm package of color picker.

--- src/ko/bindingHandlers/bindingHandlers.colorPicker.ts
+++ src/ko/bindingHandlers/bindingHandlers.colorPicker.ts
@@ -1,30 +1,33 @@
 import * as ko from "knockout";
-import * as Pickr from "pickr-widget";
+import * as Pickr from "@simonwep/pickr";
 
 ko.bindingHandlers["colorPicker"] = {
     init: (element: HTMLElement, valueAccessor) => {
         const config = valueAccessor();
 
-        const pickr = new Pickr({
+        const pickr = Pickr.create({
             el: element,
+            theme: "classic", 
+            container: element.parentElement,
             default: config.selectedColor(),
             defaultRepresentation: "HEX",
             showAlways: true,
+            useAsButton: false,
             position: "right",
             components: {
                 preview: true,
                 opacity: true,
                 hue: true,
                 interaction: {
-                    hex: true,
-                    rgba: true,
+                    hex: false,
+                    rgba: false,
                     hsva: false,
                     input: true,
                     clear: false,
                     save: false
                 }
             },
-            onChange(hsva) {
+            onChange(hsva: any): void {
                 if (config.selectedColor) {
                     config.selectedColor(hsva.toRGBA().toString());
                 }
