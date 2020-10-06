0af54e7be7414b1422d9008e2b7e1b7db3f3eb32
Alexander Zaslonov <zaslonov.alexander@gmail.com>
2020-10-05 23:02:44 -0700

    Added jsdocs to border style contracts.

--- src/contracts/borderContract.ts
+++ src/contracts/borderContract.ts
@@ -1,22 +1,71 @@
 import { StylePluginConfig } from "@paperbits/common/styles";
 
+/**
+ * Border style configuration.
+ */
 export interface BorderStyle {
+    /**
+     * Border width, e.g. "2px".
+     */
     width?: string | number;
+
+    /**
+     * Border style, e.g. "solid".
+     */
     style?: string;
-    color?: string;
+
+    /**
+     * Key of the border color.
+     */
     colorKey?: string;
 }
 
+/**
+ * Border style plugin configuration.
+ */
 export interface BorderStylePluginConfig extends StylePluginConfig {
+    /**
+     * Top border configuration.
+     */
     top?: BorderStyle;
+
+    /**
+     * Left border configuration.
+     */
     left?: BorderStyle;
+
+    /**
+     * Right border configuration.
+     */
     right?: BorderStyle;
+
+    /**
+     * Bottom border configuration.
+     */
     bottom?: BorderStyle;
 }
 
+/**
+ * Border radius style plugin configuration.
+ */
 export interface BorderRadiusStylePluginConfig extends StylePluginConfig {
+    /**
+     * Top-left radius.
+     */
     topLeftRadius?: string | number;
+
+    /**
+     * Top-right radius
+     */
     topRightRadius?: string | number;
+
+    /**
+     * Bottom-left radius
+     */
     bottomLeftRadius?: string | number;
+
+    /**
+     * Bottom-right radius
+     */
     bottomRightRadius?: string | number;
 }
