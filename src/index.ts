a7778a61c4dc759783cc81a47d4c86db9457bb3d
Alexander Zaslonov <zaslonov.alexander@gmail.com>
2020-11-14 21:29:19 -0800

    Moved local style builder to paperbits-styles module.

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,4 @@ export * from "./defaultStyleCompiler";
 export * from "./styleService";
 export * from "./styleHelper";
 export * from "./constants";
+export * from "./localStyleBuilder";
---
+++ src/localStyleBuilder.ts
@@ -0,0 +1,33 @@
+import * as Utils from "@paperbits/common/utils";
+import { StyleSheet } from "@paperbits/common/styles";
+import { IBlobStorage } from "@paperbits/common/persistence";
+import { JssCompiler } from "./jssCompiler";
+
+export class LocalStyleBuilder {
+    constructor(private readonly outputBlobStorage: IBlobStorage) { }
+
+    public async buildGlobalStyle(styleSheet: StyleSheet): Promise<void> {
+        const compiler = new JssCompiler();
+        const css = compiler.compile(styleSheet);
+        const contentBytes = Utils.stringToUnit8Array(css);
+
+        await this.outputBlobStorage.uploadBlob("styles/styles.css", contentBytes, "text/css");
+    }
+
+    public async buildLocalStyle(permalink: string, styleSheets: StyleSheet[]): Promise<void> {
+        let uploadUrl;
+
+        if (permalink === "/") {
+            uploadUrl = `/styles.css`;
+        }
+        else {
+            uploadUrl = `${permalink}/styles.css`;
+        }
+
+        const compiler = new JssCompiler();
+        const css = compiler.compile(...styleSheets);
+        const contentBytes = Utils.stringToUnit8Array(css);
+
+        await this.outputBlobStorage.uploadBlob(uploadUrl, contentBytes, "text/css");
+    }
+}
