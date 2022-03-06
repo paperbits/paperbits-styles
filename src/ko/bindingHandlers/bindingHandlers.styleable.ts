7676eb75c26bc12751837ba14a8eb2738237e728
Alexander Zaslonov <zaslonov.alexander@gmail.com>
2022-03-05 18:29:33 -0800

    tmp

--- src/ko/bindingHandlers/bindingHandlers.styleable.ts
+++ src/ko/bindingHandlers/bindingHandlers.styleable.ts
@@ -1,97 +1,22 @@
 import * as ko from "knockout";
-import * as Objects from "@paperbits/common/objects";
-import { PrimitiveContract, Styleable, StyleDefinition } from "@paperbits/common/styles";
-import { StyleService } from "../../styleService";
-import { WidgetService } from "@paperbits/common/widgets";
-import { IWidgetBinding } from "@paperbits/common/editing";
+import { Styleable } from "@paperbits/common/styles";
 
 
-export class StylableBindingHandler {
-    constructor(
-        private readonly styleService: StyleService,
-        private readonly widgetService: WidgetService
-    ) {
-        ko.bindingHandlers["styleable"] = {
-            update: async (element: HTMLElement, valueAccessor, allBindings, viewModel, bindingContext) => {
-                const config = ko.unwrap(valueAccessor());
+ko.bindingHandlers["styleable"] = {
+    init: (element: HTMLElement, valueAccessor) => {
+        const config = ko.unwrap(valueAccessor());
 
-                const styleKey: string = typeof config === "string"
-                    ? config
-                    : config.key;
+        const styleKey: string = typeof config === "string"
+            ? config
+            : config.key;
 
-                let styleable: Styleable;
+        let styleable: Styleable;
 
-                if (styleKey.includes("/instance/")) {
-                    // Temporary hack:
-                    const styleKeyParts = styleKey.split("/");
-                    const keyWithoutVariations = styleKeyParts.filter(x => x !== "default" && x !== "instance").slice(1).join("/");
-
-                    const binding: IWidgetBinding<any, any> = viewModel["widgetBinding"];
-                    const localStyles = binding.model?.styles;
-                    const handler = this.widgetService.getWidgetHandler(binding.handler);
-
-                    const styleDefinitions = handler.getStyleDefinitions();
-                    const styleDefinition = Objects.getObjectAt<StyleDefinition>(keyWithoutVariations, styleDefinitions);
-
-                    let primitive: PrimitiveContract;
-                    primitive = Objects.getObjectAt(styleKey, localStyles);
-
-                    if (!primitive) {
-                        primitive = {
-                            key: styleKeyParts.slice(2).join("/"),
-                            displayName: styleDefinition.displayName
-                        };
-
-                        Objects.mergeDeep(primitive, styleDefinition.defaults);
-                        Objects.setValue(primitive.key, localStyles, primitive);
-                    }
-
-                    styleable = {
-                        style: primitive,
-                        applyChanges: () => {
-                            viewModel["widgetBinding"].applyChanges();
-                        }
-                    };
-                }
-                else {
-                    const style = await this.styleService.getStyleByKey(styleKey);
-
-                    if (!style) {
-                        throw new Error(`Unable to find style by key ${styleKey}`);
-                    }
-
-                    const backgroundObservable = ko.observable();
-
-                    let mode = 1;
-
-                    const toggleBackground = () => {
-                        switch (mode) {
-                            case 0:
-                                backgroundObservable(null);
-                                mode = 1;
-                                break;
-                            case 1:
-                                backgroundObservable("transparent");
-                                mode = 2;
-                                break;
-
-                            case 2:
-                                backgroundObservable("dark");
-                                mode = 0;
-                                break;
-                        }
-                    };
-
-                    ko.applyBindingsToNode(element, { css: backgroundObservable }, null);
-
-                    styleable = {
-                        style: style,
-                        toggleBackground: toggleBackground
-                    };
-                }
-
-                element["styleable"] = styleable;
-            }
+        styleable = {
+            key: styleKey
         };
+
+        element["styleable"] = styleable;
     }
-}
+};
+
--- src/styles.runtime.module.ts
+++ src/styles.runtime.module.ts
@@ -5,14 +5,13 @@
  * Use of this source code is governed by a Commercial license that can be found in the LICENSE file and at https://paperbits.io/license/mit.
  */
 
+import "./ko/bindingHandlers/bindingHandlers.styleable";
 import { IInjectorModule, IInjector } from "@paperbits/common/injection";
 import { registerAnimationTriggers } from "./animationTrigger";
-import { StylableRuntimeBindingHandler } from "./ko/bindingHandlers/bindingHandlers.styleable.runtime";
 
 
 export class StyleRuntimeModule implements IInjectorModule {
     public register(injector: IInjector): void {
         registerAnimationTriggers();
-        injector.bindToCollection("autostart", StylableRuntimeBindingHandler);
     }
 }
