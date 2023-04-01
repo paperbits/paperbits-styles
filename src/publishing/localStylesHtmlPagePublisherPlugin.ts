0a07bcaeb7ba9c1c8b70fccf740547c11de92def
Alexander Zaslonov <zaslonov.alexander@gmail.com>
2023-04-01 14:58:24 -0700

    Added settings for sub-resource integrity checks.

--- src/publishing/localStylesHtmlPagePublisherPlugin.ts
+++ src/publishing/localStylesHtmlPagePublisherPlugin.ts
@@ -1,4 +1,6 @@
+import * as Utils from "@paperbits/common";
 import { MimeTypes } from "@paperbits/common";
+import { ISettingsProvider } from "@paperbits/common/configuration";
 import { Attributes } from "@paperbits/common/html";
 import { IBlobStorage } from "@paperbits/common/persistence";
 import { HtmlPagePublisherPlugin } from "@paperbits/common/publishing";
@@ -11,16 +13,26 @@ const localStylesheetFilePath = `/styles.css`;
 
 export class LocalStyleHtmlPagePublisherPlugin implements HtmlPagePublisherPlugin {
     private localStyleBuilder: StyleBuilder;
+    private subresourceIntegrityEnabled: boolean;
 
-    constructor(outputBlobStorage: IBlobStorage) {
+    constructor(
+        private readonly settingsProvider: ISettingsProvider,
+        outputBlobStorage: IBlobStorage
+    ) {
         this.localStyleBuilder = new StyleBuilder(outputBlobStorage);
+    }
+
+    private initialize = Utils.debounce(this.loadSettings.bind(this));
+
+    private async loadSettings(): Promise<void> {
+        this.subresourceIntegrityEnabled = !!await this.settingsProvider.getSetting<boolean>("features/subresourceIntegrity");
     }
 
     private appendStyleLink(document: Document, href: string, integrity: string): void {
         const element: HTMLStyleElement = document.createElement("link");
         element.setAttribute(Attributes.Href, href);
 
-        if (integrity) {
+        if (this.subresourceIntegrityEnabled && integrity) {
             element.setAttribute(Attributes.Integrity, integrity);
         }
 
@@ -31,6 +43,8 @@ export class LocalStyleHtmlPagePublisherPlugin implements HtmlPagePublisherPlugi
     }
 
     public async apply(document: Document, page: HtmlPage): Promise<void> {
+        await this.initialize();
+
         const styleManager: StyleManager = page.bindingContext?.styleManager;
 
         const localStylesheetPermalink = page.permalink === "/" // home page
